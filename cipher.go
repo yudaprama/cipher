b610dbaf15cc6d0636de04eff4f7fbc155cb2e67
[TD-Engineering] Yuda Prama <yudhaprama@icloud.com>
2020-01-20 13:46:08 +0700

    fix TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256

--- cipher.go
+++ cipher.go
@@ -4,7 +4,7 @@ import (
 	"crypto/tls"
 )
 
-var cipherMap = map[string]uint16{
+var Map = map[string]uint16{
 	"TLS_RSA_WITH_RC4_128_SHA":                tls.TLS_RSA_WITH_RC4_128_SHA,
 	"TLS_RSA_WITH_3DES_EDE_CBC_SHA":           tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
 	"TLS_RSA_WITH_AES_128_CBC_SHA":            tls.TLS_RSA_WITH_AES_128_CBC_SHA,
@@ -28,7 +28,7 @@ var cipherMap = map[string]uint16{
 	"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305":  tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
 }
 
-var cipherMapByID = map[uint16]string{
+var MapID = map[uint16]string{
 	tls.TLS_RSA_WITH_RC4_128_SHA:                "TLS_RSA_WITH_RC4_128_SHA",
 	tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA:           "TLS_RSA_WITH_3DES_EDE_CBC_SHA",
 	tls.TLS_RSA_WITH_AES_128_CBC_SHA:            "TLS_RSA_WITH_AES_128_CBC_SHA",
@@ -52,7 +52,7 @@ var cipherMapByID = map[uint16]string{
 	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305:  "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305",
 }
 
-func defaultCipher() []uint16 {
+func DefaultCipher() []uint16 {
 	return []uint16{
 		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
@@ -63,14 +63,14 @@ func defaultCipher() []uint16 {
 	}
 }
 
-var curveMap = map[string]tls.CurveID{
+var CurveMap = map[string]tls.CurveID{
 	"X25519":    tls.X25519,
 	"CurveP256": tls.CurveP256,
 	"CurveP384": tls.CurveP384,
 	"CurveP521": tls.CurveP521,
 }
 
-func defaultCurvePreferences() []tls.CurveID {
+func DefaultCurvePreferences() []tls.CurveID {
 	return []tls.CurveID{
 		tls.X25519,
 		tls.CurveP256,
